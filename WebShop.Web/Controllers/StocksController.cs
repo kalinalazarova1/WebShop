c679441254439267597e4015a7f13b899d1b5777
kalinalazarova1 <kalina.lazarova@gmail.com>
2019-02-13 21:56:08 +0000

    StocksController

--- WebShop.Web/Controllers/StocksController.cs
+++ WebShop.Web/Controllers/StocksController.cs
@@ -1,9 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using WebShop.Data;
+using WebShop.Models.Entities;
+using WebShop.Web.ViewModels;
 
 namespace WebShop.Web.Controllers
 {
@@ -11,5 +13,62 @@ namespace WebShop.Web.Controllers
     [ApiController]
     public class StocksController : ControllerBase
     {
+        private readonly WebShopContext ctx;
+        private readonly IMapper mapper;
+
+        public StocksController(WebShopContext ctx, IMapper mapper)
+        {
+            this.ctx = ctx;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var items = mapper.Map<List<StockEntryModel>>(ctx.Stock.ToList());
+            return Ok(items);
+        }
+
+        [HttpGet("{id}", Name = "StockEntryGet")]
+        public IActionResult Get(int id)
+        {
+            var item = mapper.Map<StockEntryModel>(ctx.Stock.FirstOrDefault(m => m.Id == id));
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody]StockEntryModel model)
+        {
+            this.ctx.Add(mapper.Map<StockEntry>(model));
+            if (await this.ctx.SaveChangesAsync() > 0)
+            {
+                var url = Url.Link("StockEntryGet", new { id = model.Id });
+                return Created(url, model);
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody]StockEntryModel model)
+        {
+            var oldStockEntry = this.ctx.Stock.FirstOrDefault(m => m.Id == id);
+            if (oldStockEntry == null)
+            {
+                return NotFound();
+            }
+
+            oldStockEntry.Amount = model.Amount;
+            oldStockEntry.Date = model.Date;
+            oldStockEntry.ProductId = model.ProductId;
+            oldStockEntry.StockEntryType = model.StockEntryType;
+            oldStockEntry.Units = model.Units;
+            if (await this.ctx.SaveChangesAsync() > 0)
+            {
+                return Ok(oldStockEntry);
+            }
+
+            return BadRequest();
+        }
     }
 }
