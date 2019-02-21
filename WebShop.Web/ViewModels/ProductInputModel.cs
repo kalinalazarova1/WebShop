2c2286bd8ee5ae4cba16fb1be7970b54000e8955
kalinalazarova1 <kalina.lazarova@gmail.com>
2019-02-21 00:34:50 +0000

    introducing input models

---
+++ WebShop.Web/ViewModels/ProductInputModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using WebShop.Models.Entities;
+
+namespace WebShop.Web.ViewModels
+{
+    public class ProductInputModel
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal PricePerUnit { get; set; }
+
+        public int MeasureId { get; set; }
+    }
+}
---
+++ WebShop.Web/ViewModels/StockEntryInputModel.cs
@@ -0,0 +1,15 @@
+﻿using WebShop.Models.Entities;
+
+namespace WebShop.Web.ViewModels
+{
+    public class StockEntryInputModel
+    {
+        public StockEntryType StockEntryType { get; set; }
+
+        public int ProductId { get; set; }
+
+        public decimal Units { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+}
